913773f7c4c9d7d7f42649a96031d94f0d11bc1d
Evelyne <evelynebliblis@gmail.com>
2025-03-29 18:11:55 +0300

    1

--- modules/nebula.nix
+++ modules/nebula.nix
@@ -6,18 +6,20 @@ with lib;
   options.programs.nebula = {
     enable = mkEnableOption "Enable the Nebula meta-module";
 
-    packs = {
+    options = {
       cowsay = mkOption {
         type = types.submoduleWithImports [ ./cowsay.nix ];
         default = {};
-        description = "Config for cowsay pack";
+        description = "Cowsay pack options";
       };
 
       cmatrix = mkOption {
         type = types.submoduleWithImports [ ./cmatrix.nix ];
         default = {};
-        description = "Config for cmatrix pack";
+        description = "Cmatrix pack options";
       };
     };
   };
+
+  # No `config = {}` block is needed here since submodules do their own thing
 }
