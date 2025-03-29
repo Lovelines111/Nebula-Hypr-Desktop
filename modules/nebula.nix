2071d9f43c98e9e5879f7a5bbfc312dadca9a3eb
evi <evelynebliblis@gmail.com>
2025-03-29 17:41:06 +0300

    2

--- modules/nebula.nix
+++ modules/nebula.nix
@@ -1,42 +1,23 @@
-{ config, lib, pkgs, ... }:
+{ lib, ... }:
 
 with lib;
 
-let
-  cfg = config.programs.nebula;
-in {
+{
   options.programs.nebula = {
     enable = mkEnableOption "Enable the Nebula meta-module";
 
-    cowsay = {
-      enable = mkEnableOption "Enable the cowsay pack";
-      greeting = mkOption {
-        type = types.str;
-        default = "Welcome to Nebula!";
-        description = "The cowsay greeting message.";
+    packs = {
+      cowsay = mkOption {
+        type = types.submoduleWithImports [ ./cowsay.nix ];
+        default = {};
+        description = "Config for cowsay pack";
+      };
+
+      cmatrix = mkOption {
+        type = types.submoduleWithImports [ ./cmatrix.nix ];
+        default = {};
+        description = "Config for cmatrix pack";
       };
     };
-
-    cmatrix = {
-      enable = mkEnableOption "Enable the cmatrix pack";
-      rainbow = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Enable rainbow mode (imaginary setting).";
-      };
-    };
-  };
-
-  # ✅ This is the correct top-level 'imports' field
-  imports =
-    lib.optionals cfg.enable (
-      lib.flatten [
-        (lib.optional cfg.cowsay.enable ./cowsay.nix)
-        (lib.optional cfg.cmatrix.enable ./cmatrix.nix)
-      ]
-    );
-
-  config = mkIf cfg.enable {
-    # You could set global options here if needed
   };
 }
